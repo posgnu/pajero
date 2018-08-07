27a5f30aca85a8be406a9fde640c776b50a8379b
Geunwoo Kim <posgnu@kodebox.io>
2018-08-08 00:14:44 +0900

    Implement an argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,38 @@
-extern crate rocksdb;
-use rocksdb::{DB, Writable};
+// (Full example with detailed comments in examples/01b_quick_example.rs)
+//
+// This example demonstrates clap's full 'builder pattern' style of creating arguments which is
+// more verbose, but allows easier editing, and at times more advanced options, or the possibility
+// to generate arguments dynamically.
+extern crate clap;
+use clap::{Arg, App, SubCommand};
 
 fn main() {
-    let mut db = DB::open_default("./db").unwrap();
-    //db.put(b"my key", b"my value");
-    match db.get(b"my key") {
-        Ok(Some(value)) => println!("retrieved value {}", value.to_utf8().unwrap()),
-        Ok(None) => println!("value not found"),
-        Err(e) => println!("operational problem encountered: {}", e),
+    let matches = App::new("pajero")
+                          .version("1.0")
+                          .author("GNu. <posgnu@gmail.com>")
+                          .about("Awesome packet replayer")
+                          .subcommand(SubCommand::with_name("set")
+                                      .about("Set the DB")
+                                      .version("1.0")
+                                      .author("GNu. <posgnu@gmail.com>"))
+                          .subcommand(SubCommand::with_name("play")
+                                      .about("Replay packet")
+                                      .version("1.0")
+                                      .author("GNu. <posgnu@gmail.com>")
+                                      .arg(Arg::with_name("PCAP")
+                                        .help("Path where the pcap file is located")
+                                        .required(true)))
+                          .get_matches();
+
+    match matches.subcommand() {
+        ("set", Some(sub_input)) => {}
+        ("play", Some(sub_input)) => {
+            let path = sub_input.value_of("PCAP").unwrap();
+        }
+        _ => {
+            println!("Awesome packet replayer, 1.0, GNu. <posgnu@gmail.com>")
+        }
     }
-    db.delete(b"my key");
+
+    return
 }
